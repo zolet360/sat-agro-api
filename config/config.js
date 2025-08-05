a9b86a0c98ece3e261bdbc135bfafbaecb1bf1d2
João Zolet <zolet360@gmail.com>
2025-08-05 15:48:03 -0300

    ajustes ++

--- config/config.js
+++ config/config.js
@@ -1,5 +1,3 @@
-require("dotenv").config(); // Garante que .env seja carregado
-
 module.exports = {
   development: {
     username: process.env.DB_USER || "postgres",
@@ -18,14 +16,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
-      ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
